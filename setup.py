ff1d6fb2ced1cfa9c78962603f6d750ccfd76f40
vantu03 <concaoheo203@gmail.com>
2025-06-15 02:28:33 +0700

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tiktokphoto",
-    version="1.1.6",
+    version="1.1.9",
     packages=find_packages(),
     install_requires=[
         "requests",
